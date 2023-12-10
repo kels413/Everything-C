e7aeb220c9829bb69264b1b1a56305392cab8e34
kelvin <kelvinokoye413@gmail.com>
2023-12-10 01:25:48 +0100

    first 2d array

---
+++ 2d_array/learn2d.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Drivers Code*/
+int main(void)
+{
+    int array2d[][5] =
+        {
+            {1, 2, 3, 4, 5},
+            {1, 2, 3, 4, 5},
+            {1, 2, 3, 4, 5},
+            {1, 2, 3, 4, 5},
+        };
+
+    for (int i = 0; i < 4; i++)
+    {
+        /* code */
+
+        for (int j = 0; j < 5; j++)
+        {
+            /* code */
+
+            printf("[%d]\t", array2d[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
